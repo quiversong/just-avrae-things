2aa985361a13f603aa0c3990be756ef22d74a674
quiversong <quiversong@gmail.com>
2022-06-21 15:28:09 -0600

    Add custom images to harvest.

--- util/update-harvest-data.sh
+++ util/update-harvest-data.sh
@@ -1,1 +1,3 @@
 ./spreadsheet-to-json.py "entries" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSy3EfCh9WUuUKApQKczqARxvP7-QSBxUY7YAyICP7U0lb3h8cViqtnObPS6KtI-ZNwKzr8NnwYKWUf/pub?gid=695330460&output=csv" > ../gvars/harvest-data.gvar
+
+./spreadsheet-to-json.py "images" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSy3EfCh9WUuUKApQKczqARxvP7-QSBxUY7YAyICP7U0lb3h8cViqtnObPS6KtI-ZNwKzr8NnwYKWUf/pub?gid=1816910412&output=csv" > ../gvars/harvest-images.gvar
