c0ccdcbead039361563d9d89a2fce4ee9d47c04c
quiversong <quiversong@gmail.com>
2022-07-08 08:13:43 -0600

    Basics for safari alias.

---
+++ util/update-safari-data.sh
@@ -0,0 +1,2 @@
+./spreadsheet-to-json.py "beasts" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSvaaPxosHDXFm1KgwW96S1GJMqZaYnuhNqSqJQ53Vm1qqbQeGMe_R2kXbZo-rZ6GlbQOm_HVxsOyY6/pub?gid=434315334&single=true&output=csv" > ../gvars/safari-data.gvar
+
