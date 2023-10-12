f776dc9fb83efaca8b91803eca83f87d0ac917c5
quiversong <quiversong@gmail.com>
2023-10-12 09:11:56 -0600

    Update hunt2 variables.

--- util/update-hunt-data.sh
+++ util/update-hunt-data.sh
@@ -2,8 +2,6 @@
 
 ./spreadsheet-to-json.py "mythical" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZo2JsIRDYpab3Dim7k8vytlJpwJuDtBuLsY9xGBbx3RGL3jx_-ZWvh62-op15yMiA5A3rC_-FfR13/pub?gid=826165525&single=true&output=csv" > ../gvars/hunt2-mythical.gvar
 
-./spreadsheet-to-json.py "dragons" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZo2JsIRDYpab3Dim7k8vytlJpwJuDtBuLsY9xGBbx3RGL3jx_-ZWvh62-op15yMiA5A3rC_-FfR13/pub?gid=1608511193&single=true&output=csv" > ../gvars/hunt2-battle-dragons.gvar
-
-./spreadsheet-to-json.py "mythdragons" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZo2JsIRDYpab3Dim7k8vytlJpwJuDtBuLsY9xGBbx3RGL3jx_-ZWvh62-op15yMiA5A3rC_-FfR13/pub?gid=1529406699&single=true&output=csv" > ../gvars/hunt2-mythical-dragons.gvar
+./spreadsheet-to-json.py "dragons" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZo2JsIRDYpab3Dim7k8vytlJpwJuDtBuLsY9xGBbx3RGL3jx_-ZWvh62-op15yMiA5A3rC_-FfR13/pub?gid=1608511193&single=true&output=csv" > ../gvars/hunt2-dragons.gvar
 
 ./spreadsheet-to-json.py "biomes" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZo2JsIRDYpab3Dim7k8vytlJpwJuDtBuLsY9xGBbx3RGL3jx_-ZWvh62-op15yMiA5A3rC_-FfR13/pub?gid=1597102631&single=true&output=csv" > ../gvars/hunt2-biomes.gvar
