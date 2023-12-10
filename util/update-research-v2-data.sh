31aed1515c3cc63a6330cf2363efd87af9217201
quiversong <quiversong@gmail.com>
2023-12-10 13:03:15 -0700

    add new research data.

---
+++ util/update-research-v2-data.sh
@@ -0,0 +1,3 @@
+./spreadsheet-to-json.py "spells" "https://docs.google.com/spreadsheets/d/e/2PACX-1vRrHfD_SuvDuKQS8srF79fGa8oAdfXPwFPoWbglQRWEIogep3D_2naD1WjQ8s36nOKWt8K4u69nuaTM/pub?gid=1003011196&single=true&output=csv" > ../gvars/research-spells.gvar
+
+./spreadsheet-to-json.py "spells" "https://docs.google.com/spreadsheets/d/e/2PACX-1vRrHfD_SuvDuKQS8srF79fGa8oAdfXPwFPoWbglQRWEIogep3D_2naD1WjQ8s36nOKWt8K4u69nuaTM/pub?gid=1263868032&single=true&output=csv" > ../gvars/research-spell-chances.gvar
